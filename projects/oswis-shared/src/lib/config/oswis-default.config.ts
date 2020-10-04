fb38ec000ed81f61ca1d9d77db6b183631c57abd
zakjakub <mail@jakubzak.eu>
2020-10-04 18:50:28 +0200

    Reformat...

--- projects/oswis-shared/src/lib/config/oswis-default.config.ts
+++ projects/oswis-shared/src/lib/config/oswis-default.config.ts
@@ -70,8 +70,8 @@ export abstract class OswisDefaultConfig implements OswisConfig {
     const blackListedDomains = [];
     this.getDomains().map(
       (domain: String) => {
-       blackListedDomains.push('//' + domain + '/api/login_check');
-       blackListedDomains.push('//' + domain + '/api/token/refresh');
+        blackListedDomains.push('//' + domain + '/api/login_check');
+        blackListedDomains.push('//' + domain + '/api/token/refresh');
       }
     );
 
